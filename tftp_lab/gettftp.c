79acadd1b0a6fafb7eba5da6b3dc88c7f2f31114
Kush55 <21052766@kiit.ac.in>
2024-12-19 10:20:27 +0100

    gettftp modified

--- tftp_lab/gettftp.c
+++ tftp_lab/gettftp.c
@@ -4,98 +4,90 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
+#include <netdb.h> // For getaddrinfo
 
-#define TFTP_PORT 69
-#define BUFFER_SIZE 512
+#define PORT "69"
+#define MAX_PACKET_SIZE 516
+#define BLOCK_SIZE 512
 
-// TFTP opcodes
-#define RRQ 1   // Read request
-#define WRQ 2   // Write request
-#define DATA 3  // Data packet
-#define ACK 4   // Acknowledgement
-#define ERROR 5 // Error packet
-
-// Function to create the TFTP request packet
-void create_rrq_packet(char *filename, char *packet) {
-    packet[0] = 0;  // Zero byte for TFTP
-    packet[1] = RRQ; // Read Request
-    strcpy(&packet[2], filename);
-    memset(&packet[2 + strlen(filename)], 0, 1); // Null-terminate the filename
-    strcpy(&packet[3 + strlen(filename)], "octet"); // Mode: octet (binary)
-    memset(&packet[3 + strlen(filename) + 5], 0, 1); // Null-terminate "octet"
+void error(const char *msg) {
+    perror(msg);
+    exit(1);
 }
-// Function to receive data from the TFTP server
-void receive_file(int sockfd, struct sockaddr_in server_addr, char *filename) {
-    char buffer[BUFFER_SIZE];
-    FILE *file = fopen(filename, "wb");
-    if (!file) {
-        perror("Failed to open file for writing");
-        return;
-    }
+void getftp(const char *server_name, const char *file_name) {
+    int sockfd;
+    struct addrinfo hints, *server_info, *p;
+    struct sockaddr_in from_addr;
+    socklen_t addr_len = sizeof(from_addr);
+    char buffer[MAX_PACKET_SIZE], ack[4];
+    int block_number = 1;
+    ssize_t received_bytes;
+    // Use getaddrinfo to resolve server address
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;       // IPv4
+    hints.ai_socktype = SOCK_DGRAM;  // UDP
 
-    int block_num = 1;
-    while (1) {
-        int len = recvfrom(sockfd, buffer, BUFFER_SIZE, 0, NULL, NULL);
-        if (len < 0) {
-            perror("Failed to receive data");
+    if (getaddrinfo(server_name, PORT, &hints, &server_info) != 0)
+        error("Failed to resolve server address");
+    // Create UDP socket
+    for (p = server_info; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) >= 0)
             break;
-        }
+    }
+    if (p == NULL)
+        error("Socket creation failed");
 
-        // Check if it's a data packet (opcode 3)
-        if (buffer[1] == DATA) {
-            // Write the data to the file
-            fwrite(&buffer[4], 1, len - 4, file);
 
-            // Send an acknowledgment for the received block
-            buffer[0] = 0;
-            buffer[1] = ACK;
-            buffer[2] = (block_num >> 8) & 0xFF;
-            buffer[3] = block_num & 0xFF;
-            sendto(sockfd, buffer, 4, 0, (struct sockaddr *)&server_addr, sizeof(server_addr));
+    // Prepare RRQ (Read Request) packet
+    sprintf(buffer, "%c%s%c%s%c", 0x00, file_name, 0x00, "octet", 0x00);
+    size_t len = 2 + strlen(file_name) + 1 + strlen("octet") + 1;
 
-            if (len < BUFFER_SIZE) {
-                break; // Last block received
-            }
+    // Send RRQ to server
+    if (sendto(sockfd, buffer, len, 0, p->ai_addr, p->ai_addrlen) < 0)
+        error("Failed to send RRQ");
 
-            block_num++;
+    FILE *file = fopen(file_name, "wb");
+    if (!file)
+        error("Failed to create file");
+
+    while (1) {
+        // Receive DATA packet
+        received_bytes = recvfrom(sockfd, buffer, MAX_PACKET_SIZE, 0, (struct sockaddr *)&from_addr, &addr_len);
+        if (received_bytes < 4)
+            error("Packet too short");
+
+        int opcode = ntohs(*(short *)buffer);
+        int received_block = ntohs(*(short *)(buffer + 2));
+
+        if (opcode == 3 && received_block == block_number) { // DATA packet
+            fwrite(buffer + 4, 1, received_bytes - 4, file);
+
+            // Send ACK
+            *(short *)ack = htons(4);
+            *(short *)(ack + 2) = htons(block_number);
+            sendto(sockfd, ack, 4, 0, (struct sockaddr *)&from_addr, addr_len);
+            block_number++;
+            if (received_bytes < MAX_PACKET_SIZE) // End of file
+                break;
+        } else if (opcode == 5) { // ERROR packet
+            fprintf(stderr, "Error received: %s\n", buffer + 4);
+            fclose(file);
+            close(sockfd);
+            freeaddrinfo(server_info);
+            exit(1);
         }
     }
-
+    printf("File \"%s\" downloaded successfully.\n", file_name);
     fclose(file);
+    close(sockfd);
+    freeaddrinfo(server_info); // Free the server info
 }
-// Main function for downloading a file
+
 int main(int argc, char *argv[]) {
     if (argc != 3) {
-        fprintf(stderr, "Usage: %s <server> <filename>\n", argv[0]);
+        printf("Usage: %s <server_name> <file_name>\n", argv[0]);
         return 1;
     }
-
-    char *server_ip = argv[1];
-    char *filename = argv[2];
-    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sockfd < 0) {
-        perror("Failed to create socket");
-        return 1;
-    }
-
-    struct sockaddr_in server_addr;
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(TFTP_PORT);
-    if (inet_pton(AF_INET, server_ip, &server_addr.sin_addr) <= 0) {
-        perror("Invalid server address");
-        return 1;
-    }
-
-    // Send RRQ packet
-    char packet[BUFFER_SIZE];
-    create_rrq_packet(filename, packet);
-    sendto(sockfd, packet, sizeof(packet), 0, (struct sockaddr *)&server_addr, sizeof(server_addr));
-
-    // Receive the file from the TFTP server
-    receive_file(sockfd, server_addr, filename);
-
-    close(sockfd);
+    getftp(argv[1], argv[2]);
     return 0;
 }
